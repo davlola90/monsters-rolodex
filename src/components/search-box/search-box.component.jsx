6df459f43dacaa5da9ffac817a4441844a15058a
Aymen Toukabri <aymentoukabri1990@outlook.com>
2023-01-01 10:03:14 +0100

    rolodex projetc

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,26 @@
+import "./search-box.styles.css";
+
+const SearchBox = ({onChangeHandler, placeholder, className}) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+/*class SearchBox extends Component {
+  render() {
+    const { onChangeHandler, placeholder, className } = this.props;
+
+    return (
+      <input
+        className={`search-box ${className}`}
+        type="search"
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+      />
+    );
+  }
+}*/
+export default SearchBox;
